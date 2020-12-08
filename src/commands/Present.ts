b6a78f7f9b0772546e83153cdbc2c7263f9edcfd
Chris-Bitler <chris@cbitler.io>
2020-12-07 23:19:47 -0500

    Add more tiger images

--- src/commands/Present.ts
+++ src/commands/Present.ts
@@ -15,7 +15,18 @@ const IMAGES = [
     "https://cdn.discordapp.com/attachments/535311735083761705/784621940501118986/xmas8_t1160.png",
     "https://cdn.discordapp.com/attachments/535311735083761705/784621967902507069/Tiger2Bgets2Bin2Ba2Btangle2Bwith2Bher2Bearly2BChristmas2Bpresent2B3.png",
     "https://cdn.discordapp.com/attachments/535311735083761705/784622011879784518/eggmhud3w3631.png",
-    "https://cdn.discordapp.com/attachments/535311735083761705/784622331796127754/61bd4c3d64ba113e5bfe3a33517ad097.png"
+    "https://cdn.discordapp.com/attachments/535311735083761705/784622331796127754/61bd4c3d64ba113e5bfe3a33517ad097.png",
+    "https://cdn.discordapp.com/attachments/687444272458629194/785366739210207232/121217_tiger_presents_web.png",
+    "https://cdn.discordapp.com/attachments/687444272458629194/785367478859071498/bb4ff8de-03e0-44c5-8935-e72884fefe7d.png",
+    "https://cdn.discordapp.com/attachments/687444272458629194/785367594122870804/maxresdefault.png",
+    "https://cdn.discordapp.com/attachments/778482708212088853/785721567421792286/image.png",
+    "https://cdn.discordapp.com/attachments/778482708212088853/785721665409384478/f4fc57911b4f490c0d358d4852a9a2a5.png",
+    "https://cdn.discordapp.com/attachments/778482708212088853/785721685617147904/vli8j-white-tiger-plays-in-box-lg.png",
+    "https://cdn.discordapp.com/attachments/778482708212088853/785721709625344010/QE7nITJ.png",
+    "https://cdn.discordapp.com/attachments/778482708212088853/785721789387898920/Puss2Bin2Bbox25212BTiger2Bgets2Bin2Ba2Btangle2Bwith2Bher2Bearly2BChristmas2Bpresent2B2B2B4.png",
+    "https://cdn.discordapp.com/attachments/778482708212088853/785721810943213598/IgCOGWL.png",
+    "https://cdn.discordapp.com/attachments/778482708212088853/785721836519424000/maxresdefault.png",
+    "https://cdn.discordapp.com/attachments/778482708212088853/785721865796452362/maxresdefault.png"
 ];
 
 export class Present extends Command {
